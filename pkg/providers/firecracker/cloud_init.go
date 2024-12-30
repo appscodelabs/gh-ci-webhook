1d99aa97c7fac0083062a0e028979d8a86922177
Tamal Saha <tamal@appscode.com>
2024-12-30 00:32:58 -0800

    Install gh cli
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/providers/firecracker/cloud_init.go
+++ pkg/providers/firecracker/cloud_init.go
@@ -234,6 +234,17 @@ for IMAGE_NAME in tonistiigi/binfmt:latest moby/buildkit:buildx-stable-1; do
 	done
 done
 
+# install github cli
+# https://github.com/cli/cli/blob/trunk/docs/install_linux.md
+(type -p wget >/dev/null || (sudo apt update && sudo apt-get install wget -y)) \
+	&& sudo mkdir -p -m 755 /etc/apt/keyrings \
+        && out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
+        && cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
+	&& sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg \
+	&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+	&& sudo apt update \
+	&& sudo apt install gh -y
+
 # Prepare GitHun Runner user
 export USER=runner
 # https://docs.docker.com/engine/install/linux-postinstall/
